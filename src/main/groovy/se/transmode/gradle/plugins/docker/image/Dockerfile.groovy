b40a5c6f1faa3bb0c9431c138fa4aad33e569e78
Matthias Grüter <matthias.gruter@transmode.com>
2014-07-26 22:11:08 +0200

    New Dockerfile class (with tests).

---
+++ src/main/groovy/se/transmode/gradle/plugins/docker/image/Dockerfile.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Transmode AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.transmode.gradle.plugins.docker.image
+
+class Dockerfile {
+    List<String> instructions
+
+    Dockerfile(List<String> instructions=[]) {
+        this.instructions = instructions
+    }
+
+    void append(String instruction) {
+        this.instructions.add(instruction)
+    }
+
+    void writeToFile(File destination) {
+        destination.withWriter { out ->
+            instructions.each() { line ->
+                out.writeLine(line)
+            }
+        }
+    }
+
+    static Dockerfile fromExternalFile(File source) {
+        def dockerfile = new Dockerfile()
+        source.eachLine {
+            dockerfile.append(it)
+        }
+        return dockerfile
+    }
+
+    static Dockerfile fromBaseImage(String base) {
+        return new Dockerfile(["FROM ${base}"])
+    }
+}
---
+++ src/test/groovy/se/transmode/gradle/plugins/docker/image/DockerfileTest.groovy
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 Transmode AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.transmode.gradle.plugins.docker.image
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.equalTo
+
+class DockerfileTest {
+
+    public static final String BASE_IMAGE = 'ubuntu:14.04'
+    public static final String MAINTAINER = 'MAINTAINER john doe'
+    public static final ArrayList<String> INSTRUCTIONS = [
+            'FROM debian:jessie',
+            'CMD /bin/bash'
+    ]
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder()
+
+    private File createTestDockerfile() {
+        def source = testFolder.newFile('Dockerfile')
+        source.withWriter { out ->
+            INSTRUCTIONS.each { out.writeLine(it) }
+        }
+        return source
+    }
+
+    @Test
+    void createFromBase() {
+        assertThat(Dockerfile.fromBaseImage(BASE_IMAGE).instructions as ArrayList<GString>,
+                equalTo(["FROM ${BASE_IMAGE}"]))
+    }
+
+    @Test
+    void createFromBaseAndAppend() {
+        def dockerfile = Dockerfile.fromBaseImage(BASE_IMAGE)
+        dockerfile.append(MAINTAINER)
+        assertThat(dockerfile.instructions as ArrayList<String>,
+                equalTo(["FROM ${BASE_IMAGE}", MAINTAINER]))
+    }
+
+    @Test
+    void createFromFile() {
+        File source = createTestDockerfile()
+        def dockerfile = Dockerfile.fromExternalFile(source)
+        assertThat(dockerfile.instructions as ArrayList<String>,
+                equalTo(INSTRUCTIONS))
+    }
+
+    @Test
+    void createFromFileAndAppend() {
+        File source = createTestDockerfile()
+        def dockerfile = Dockerfile.fromExternalFile(source)
+        dockerfile.append(MAINTAINER)
+        assertThat(dockerfile.instructions,
+                equalTo(INSTRUCTIONS + [MAINTAINER]))
+    }
+}
